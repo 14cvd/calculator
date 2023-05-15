6e6648b8715899425d26b2a8a26fa53c98127c80
Cavid <84477254+14cvd@users.noreply.github.com>
2023-05-14 17:51:06 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:calculator/mainPage.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isSwitch = false;
+  void ChangeSwitch() {
+    setState(() {
+      isSwitch = !isSwitch;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: MainPage(),
+      ),
+    );
+  }
+}
